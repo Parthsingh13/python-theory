fb0d132a2279e998f17cfb57bbd7bb8335016c3d
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-03-05 12:19:35 +0530

    Add files via upload

---
+++ Distance (km to m).py
@@ -0,0 +1,7 @@
+""" Convert the total distance in kilometre and metre """
+
+d=int(input("Enter the distance in metre :"))
+k=d//1000
+m=d%1000
+print("Distance i km :",k)
+print("Distance i metre :",m)
