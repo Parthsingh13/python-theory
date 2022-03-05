141730e4f2f33221c30ecd1eed55a1053d7e18fa
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-03-05 12:22:20 +0530

    Add files via upload

---
+++ Maximum number.py
@@ -0,0 +1,5 @@
+""" Find the maximum number between two numbers """
+
+print("Enter the two numbers")
+a,b=int(input("Enter the  first number :")),int(input("Enter the secand number :"))
+print("Mximum number :",max(a,b))
