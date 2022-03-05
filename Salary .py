987aa97cfeddb9f16532e578d3f43fa41541c1f8
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-03-05 12:23:24 +0530

    Add files via upload

---
+++ Salary .py
@@ -0,0 +1,18 @@
+""" To calculate the total salary of the employee with the help of Basic salary(HRA,DA...etc) """
+
+Basic=int(input("Enter the basic salary of employee:"))
+if(Basic<=10000):
+    HRA=(80/100)*Basic
+    DA=(90/100)*Basic
+    Total=HRA+DA+Basic
+    print("Total salary of employee is:" ,Total)
+elif(10000<Basic<=20000):
+    HRA=(85/100)*Basic
+    DA=(90/100)*Basic
+    Total=HRA+DA+Basic
+    print("Total salary of employee is:" ,Total)
+else:
+    HRA=(95/100)*Basic
+    DA=(95/100)*Basic
+    Total=HRA+DA+Basic
+    print("Total salary of employee is:" ,Total)
