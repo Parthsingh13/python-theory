d8201dd0515a87818b2f6c9f52c0def841b91e69
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-05-31 00:22:01 +0530

    Add files via upload

---
+++ Arguement with return.py
@@ -0,0 +1,10 @@
+"""      Arguement with return     """
+
+def no():
+    if sorted(s1)==sorted(s2):
+        return("Anagram")
+    else:
+        return("Not Anagram")
+s1=input("Enter the String:")
+s2=input("Enter the string2:")
+print(no())
