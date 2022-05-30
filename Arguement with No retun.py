2e45dd034b94fe5f52f412663284756b9578b28f
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-05-31 00:21:45 +0530

    Add files via upload

---
+++ Arguement with No retun.py
@@ -0,0 +1,10 @@
+"""      Arguement with No retun     """
+
+def no():
+    if sorted(s1)==sorted(s2):
+        print("Anagram")
+    else:
+        print("Not Anagram")
+s1=input("Enter the String:")
+s2=input("Enter the string2:")
+no()
