ff50502e1faa40807f89a949ab3fbd07e905b4a9
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-06-01 12:22:06 +0530

    Add files via upload

---
+++ Remove empty string from list.py
@@ -0,0 +1,7 @@
+"""  Remove Empty string from list    """
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+
+# remove None from list1 and convert result into list
+res = list(filter(None, list1))
+print(res)
