8fed5cf1b7ce019fb0c3e0744c9dedad71a37ddb
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-06-01 11:42:58 +0530

    Add files via upload

---
+++ merge 2 dict into 1.py
@@ -0,0 +1,7 @@
+"""  Merge 2 python Dictionary into 1"""
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
